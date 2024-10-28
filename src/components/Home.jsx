55730fc313c06d49c3a4398802e7f6636877f374
harsha vardhan <harshavardhanmanne410@gmail.com>
2024-10-28 11:43:56 +0530

    job update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
       <div className="flex flex-col items-center md:items-start">
         <h1 className="text-center md:text-left">Hello, I'm</h1>
         <h1 className="text-3xl mb-3 mt-2 text-center md:text-left">Manne Harsha Vardhan</h1>
-        <h2 className="text-2xl font-light text-center md:text-left">Software Developer</h2>
+        <h2 className="text-2xl font-light text-center md:text-left">Software Developer At Amazon</h2>
         <div className="flex mt-6 justify-center md:justify-start">
           <button className="px-4 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mr-5" onClick={downloadResume}>
             Download CV

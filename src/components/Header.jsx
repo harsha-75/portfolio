13d8a8327cdbc9408a3e5c501c508a354bfc9862
harsha vardhan <harshavardhanmanne410@gmail.com>
2024-10-28 12:01:08 +0530

    ui changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 function Header() {
-  const navigate= useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Function to check if the current path matches the provided path
+  const isActive = (path) => location.pathname === path;
+
   return (
     <header className='relative bg-slate-700 text-white py-1 md:py-3'>
-    <ul className='flex justify-center md:justify-end pt-3 pb-3'>
-      <li className="mr-5 md:mr-10 hover:text-yellow-500" ><button onClick={
-             ()=>{
-                   navigate('/home')
-             }
-        }>
-          Home
-        </button>
+      <ul className='flex justify-center md:justify-end pt-3 pb-3'>
+        <li className="mr-5 md:mr-10">
+          <button 
+            onClick={() => navigate('/home')}
+            className={isActive('/home') ? 'text-yellow-500' : 'hover:text-yellow-500'}
+          >
+            Home
+          </button>
         </li>
-      <li className='mr-5 md:mr-10 hover:text-yellow-500' ><button onClick={()=> navigate('/about')}>
-           About
-        </button></li>
-      <li className='mr-5 md:mr-10 hover:text-yellow-500'><button onClick={()=> navigate('/project')}>
-           Projects
-        </button></li>
-      <li className='mr-5 md:mr-10 hover:text-yellow-500'><button onClick={()=> navigate('/contact')}>
-           Contact
-        </button></li>
-    </ul>
-  </header>
-  )
+        <li className="mr-5 md:mr-10">
+          <button 
+            onClick={() => navigate('/about')}
+            className={isActive('/about') ? 'text-yellow-500' : 'hover:text-yellow-500'}
+          >
+            About
+          </button>
+        </li>
+        <li className="mr-5 md:mr-10">
+          <button 
+            onClick={() => navigate('/project')}
+            className={isActive('/project') ? 'text-yellow-500' : 'hover:text-yellow-500'}
+          >
+            Projects
+          </button>
+        </li>
+        <li className="mr-5 md:mr-10">
+          <button 
+            onClick={() => navigate('/contact')}
+            className={isActive('/contact') ? 'text-yellow-500' : 'hover:text-yellow-500'}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
